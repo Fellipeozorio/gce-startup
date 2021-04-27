8a5814bb7c87884a219fc3fe6b9785a14aea42a3
Fellipeozorio <62856548+Fellipeozorio@users.noreply.github.com>
2021-04-27 16:13:31 -0300

    Update gce-startup.sh

--- gce-startup.sh
+++ gce-startup.sh
@@ -209,12 +209,13 @@ gcsfuse $BUCKET_R /mnt/gcs-bucket-R
 # You can then install R using the following command:
 # update indices
 apt update -qq
-# install two helper packages we need
 apt install --no-install-recommends software-properties-common dirmngr
-# import the signing key (by Michael Rutter) for these repo
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
 add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt install --no-install-recommends r-base
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo apt install --no-install-recommends r-cran-rstan
+
 sudo apt-get install r-base
 
 # rstudio
